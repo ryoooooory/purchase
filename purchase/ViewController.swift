eac57b263cf5ad5b42ab3050bc067ca204eba294
大嶋涼 <ryoooooory@docomo.ne.jp>
2019-02-14 14:48:21 +0900

    az

---
+++ purchase/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 大嶋　涼 on 2019/02/01.
+//  Copyright © 2019年 大嶋　涼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var unko = 11
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //ssrdr
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
