cadd0a632df75144e51a6b115228a38f7059f31d
大嶋涼 <ryoooooory@docomo.ne.jp>
2019-02-16 12:21:11 +0900

    ryo

--- purchase/ListviewcController.swift
+++ purchase/ListviewcController.swift
@@ -11,11 +11,18 @@ import UIKit
 import Firebase
 
 
-class ChatTargetUserCell: UITableViewCell {
+class ChatTargetUserCell:UITableViewCell{
     @IBOutlet weak var nameLabel:UILabel!
     
     func bind(item:Item){
-        self.nameLabel.text = ("\(item.name)  \(item.num)個")
+        print(item.name)
+        
+        self.nameLabel.text = ("\(item.name)  \(item.price)円" )
+    }
+    
+    
+    func sumdata(item:Item) -> Int{
+        return item.price
     }
 }
 
@@ -24,36 +31,84 @@ class ChatTargetUserCell: UITableViewCell {
 
 class ListviewController: UITableViewController{
     
-    var cellnum = 0;
     var items = [Item]()
+    var countitems = [Item]()                   //データ数を数える用
+    
     var ref: DatabaseReference!
+    var counter = 1;                    //セルの番号
+    var sum = 0;                        //合計金額
+    var barcode = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("List")
-        //        makeList()
-    }
-    //表示するセルの数
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
-    }
-    
-    //セルの作成
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ChatTargetUserCell
-        let item = self.items[indexPath.row]
-        cell.bind(item: item)
-        return cell
+        print("start")
+        counter = 1
+        if(barcode != ""){                      //バーコード読み取って購入履歴に追加する時。
+            self.countList()
+        }else{                                  //購入履歴から来た時。
+            self.makeList()
+        }
     }
     
-    //情報表示
-    func makeList(){
-        self.items = []
+    
+    
+    //今あるデータ数をカウントしてcountitemに入れていく
+    func countList(){
         ref = Database.database().reference()
-        
-        
+        print("countstart")
         //情報取得
-        ref.child("user").observe(DataEventType.value) { (snapshot) in
+        ref.child("user").observeSingleEvent(of: DataEventType.value, with: {(snapshot)  in          //userより下のデータをsnapshotで取得
+            self.countitems = [Item]()
+            for data in snapshot.children{
+                if let snap = data as? DataSnapshot{
+                    print(data)
+                    let dataitem = Item(snapshot: snap)
+                    self.countitems.append(dataitem)
+                }
+            }
+            print("countend")
+            self.finditem()
+        })
+    }
+    
+    
+    
+    
+    
+    //商品登録のところからbarcodeの商品を探し出す、そしてcountitemの数を元にidを決定し更新
+    func finditem(){
+        print("findstart")
+        ref = Database.database().reference()
+        ref.child("data/\(barcode)").observeSingleEvent(of: DataEventType.value, with: {(snapshot)  in          //userより下のデータをsnapshotで取得
+            var id = self.countitems.count
+            
+            
+            let dataitem = Item(snapshot: snapshot)
+            
+            
+            let updataname = ["name":dataitem.name]
+            let updataprice = ["price":dataitem.price]
+            
+            
+            self.ref.child("user/\(id+1)").updateChildValues(updataname)        //idを取得して、データベースに追加
+            self.ref.child("user/\(id+1)").updateChildValues(updataprice)        //idを取得して、データベースに追加
+            
+            print("uploadfinish")
+            print("findend")
+            self.makeList()
+            
+        })
+        
+    }
+    
+    
+    //itemsにデータを入れていく
+    func makeList(){
+        ref = Database.database().reference()
+        print("make")
+        //情報取得
+        ref.child("user").observeSingleEvent(of: DataEventType.value, with: {(snapshot)  in          //userより下のデータをsnapshotで取得
             self.items = [Item]()
             for data in snapshot.children{
                 if let snap = data as? DataSnapshot{
@@ -61,10 +116,59 @@ class ListviewController: UITableViewController{
                     self.items.append(dataitem)
                 }
             }
+            print("makeend")
+            self.tableView.reloadData()                 //cellをつくり始める
             
-            self.tableView.reloadData()
-        }
+        })
+    }
+    
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("appear")
+     //   tableView.reloadData()
+        
         
     }
+    
+    
+    //表示するセルの数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("tablecount")
+        print(items.count)
+        return items.count+1                       //最後のセルは合計金額を出す
+        
+    }
+    
+    
+    //セルの作成、最後に呼ばれる
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print("table")
+        print(counter)
+        if counter == items.count+1{                //最後のセルだったら合計金額をセルのtextに入れる
+            
+            // let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            // let cxell = tableView.cellForRow(at: indexPath)
+            //  var cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ChatTargetUserCell
+            cell.textLabel?.text = "合計\(sum)円"
+            
+            return cell
+            
+        }else{                                      //それ以外はFirebaseから取得したものを入れていく
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ChatTargetUserCell
+            let item = self.items[indexPath.row]
+            cell.bind(item: item)                           //Firebaseから取得したデータ(items)からそれぞれの要素に分解
+            counter += 1                                    //cellが何番目かカウント
+            sum += cell.sumdata(item: item)                 //合計金額にプラスしていく
+            
+            return cell
+        }
+    }
+    
+    
     
 }
